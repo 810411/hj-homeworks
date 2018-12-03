d3fb7442e6c17013ed87836c2e2c9b5e572c5d6b
Grigorii Balabanov <810411@mail.ru>
2018-12-03 20:51:59 +0500

    slider fix

--- browser/slider/js/index.js
+++ browser/slider/js/index.js
@@ -11,9 +11,10 @@ const slider = document.getElementById('slider');
 
 let step = 0;
 
-setInterval(() => {
-    slider.src = `./i/${fileNames[step]}`;
-    step = (step >= 4) ? 0 : ++step;
-  },
-  5000
-);
+function setSlider() {
+  slider.src = `./i/${fileNames[step]}`;
+  step = (step >= 4) ? 0 : ++step;
+}
+
+setSlider();
+setInterval(setSlider, 5000);
