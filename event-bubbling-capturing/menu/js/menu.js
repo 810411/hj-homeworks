a9d50f7b8bddcde961b0e1cc591452c4ad2ed70a
Grigorii Balabanov <810411@mail.ru>
2018-12-19 19:35:20 +0500

    event-bubbling-capturing done

--- event-bubbling-capturing/menu/js/menu.js
+++ event-bubbling-capturing/menu/js/menu.js
@@ -1,17 +1,23 @@
 'use strict';
 
 function toggleMenu(event) {
-  if (this.classList.contains('show')) {
-    this.classList.remove('show');
-    this.classList.add('hide');
+  const current = event.currentTarget;
+
+  if (current.classList.contains('show')) {
+    current.classList.remove('show');
+    current.classList.add('hide');
   } else {
-    this.classList.add('show');
-    this.classList.remove('hide');
+    current.classList.add('show');
+    current.classList.remove('hide');
   }
 }
 
 function openLink(event) {
+  event.preventDefault();
+
   console.log(this.textContent);
+
+  event.stopPropagation();
 }
 
 function init(node) {
--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,14 @@
 'use strict';
+
+list.addEventListener('click', () => {
+  event.preventDefault();
+
+  if (event.target.classList.contains('add-to-cart')) {
+    const item = {
+      title: event.target.dataset.title,
+      price: event.target.dataset.price
+    };
+
+    addToCart(item);
+  }
+});
