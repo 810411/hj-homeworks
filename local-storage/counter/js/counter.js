c5cfa6eff50f046fee9ebe55756a42cf0f600aeb
Grigorii Balabanov <810411@mail.ru>
2018-12-20 18:06:08 +0500

    local-storage done

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const
+  counter = document.getElementById('counter'),
+  btns = document.querySelector('.wrap-btns');
+
+if (localStorage.count === undefined) {
+  localStorage.count = 0;
+}
+
+counter.textContent = localStorage.count;
+
+btns.addEventListener('click', event => {
+  if (event.target.id === ('increment')) {
+    counter.textContent++;
+  } else if (event.target.id === ('decrement')) {
+    if (counter.textContent > 0) {
+      counter.textContent--;
+    }
+  } else if (event.target.id === ('reset')) {
+    counter.textContent = 0;
+  }
+
+  localStorage.count = counter.textContent;
+});
