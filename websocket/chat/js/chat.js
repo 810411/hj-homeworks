acddf0a5dab6babcede4d3440dcd5dcc0bf86078
Grigorii Balabanov <810411@mail.ru>
2018-12-26 17:45:06 +0500

    websocket done

--- websocket/chat/js/chat.js
+++ websocket/chat/js/chat.js
@@ -1,1 +1,75 @@
 'use strict';
+
+const
+  connection = new WebSocket('wss://neto-api.herokuapp.com/chat'),
+  chat = document.querySelector('.chat'),
+  chatElems = init(chat);
+
+function init(chatElem) {
+  return {
+    form: chatElem.querySelector('.message-box'),
+    input: chatElem.querySelector('.message-input'),
+    submit: chatElem.querySelector('.message-submit'),
+    content: chatElem.querySelector('.messages-content'),
+    status: chatElem.querySelector('.chat-status'),
+    templates: {
+      messageUser: chatElem.querySelector('.message.loading + .message'),
+      messageMy: chatElem.querySelector('.message.message-personal'),
+      loading: chatElem.querySelector('.message.loading').cloneNode(true),
+      status: chatElem.querySelector('.message.message-status')
+    }
+  };
+}
+
+function triggerStatus(messageStatus) {
+  const message = chatElems.templates.status.cloneNode(true);
+  message.querySelector('.message-text').textContent = messageStatus;
+  chatElems.content.appendChild(message);
+}
+
+function addMessage(who, data) {
+  const time = new Date();
+  who.querySelector('.message-text').textContent = data;
+  who.querySelector('.timestamp').textContent = time.toLocaleTimeString('ru-RU', {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+}
+
+connection.addEventListener('open', () => {
+  triggerStatus('Пользователь появился в сети');
+  chatElems.status.textContent = chatElems.status.dataset.online;
+  chatElems.submit.disabled = false;
+});
+
+connection.addEventListener('close', () => {
+  triggerStatus('Пользователь не в сети');
+  chatElems.status.textContent = chatElems.status.dataset.offline;
+  chatElems.submit.disabled = true;
+});
+
+connection.addEventListener('message', (event) => {
+  const data = event.data;
+  const typingMessage = chatElems.templates.loading.cloneNode;
+  const messageUser = chatElems.templates.messageUser.cloneNode(true);
+
+  if (data === '...') {
+    chatElems.content.appendChild(typingMessage).scrollIntoView({block: "end", behavior: "smooth"});
+  } else {
+    addMessage(messageUser, data);
+    chatElems.content.typingMessage ? chatElems.content.removeChild(typingMessage) : null;
+    chatElems.content.appendChild(messageUser).scrollIntoView({block: "end", behavior: "smooth"});
+  }
+});
+
+chatElems.form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const value = chatElems.input.value;
+  const messageMy = chatElems.templates.messageMy.cloneNode(true);
+
+  addMessage(messageMy, value);
+  chatElems.content.appendChild(messageMy).scrollIntoView({block: "end", behavior: "smooth"});
+  connection.send(value);
+  chatElems.form.reset();
+});
--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const
+  connection = new WebSocket('wss://neto-api.herokuapp.com/counter'),
+  counter = document.querySelector('.counter'),
+  outputErrors = document.querySelector('output.errors');
+
+window.addEventListener('beforeunload', () => {
+  connection.onclose = function () {};
+  connection.close(1000);
+});
+
+connection.addEventListener('message', event => {
+  const message = JSON.parse(event.data);
+  counter.textContent = message.connections;
+  outputErrors.textContent = message.errors;
+});
